b6d3dfeb8c5c4d7e047cc4d751229f6631a2071a
OliaSalamatina <99215497+OliaSalamatina@users.noreply.github.com>
2025-04-09 16:54:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
 variable "config" {
+  type = object({
+    metadata = object({
+      project    = string
+      owner      = string
+      department = string
+      tags       = list(string)
+    })
+    feature_toggles = list(object({
+      name    = string
+      enabled = bool
+    }))
+  })
+
   default = {
     metadata = {
       project     = "internal-dashboard"
