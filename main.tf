6fbeb9dfb9f67256e65be3cb23de2cf6164aba91
OliaSalamatina <99215497+OliaSalamatina@users.noreply.github.com>
2025-04-09 16:51:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,29 @@
-variable "greeting" {
-  type    = string
-  default = "Hello from Scalr!"
+variable "config" {
+  default = {
+    metadata = {
+      project     = "internal-dashboard"
+      owner       = "team-devops"
+      department  = "IT"
+      tags        = ["internal", "high-priority"]
+    }
+
+    feature_toggles = [
+      {
+        name    = "enable_user_audit"
+        enabled = true
+      },
+      {
+        name    = "allow_guest_login"
+        enabled = false
+      },
+      {
+        name    = "beta_mode"
+        enabled = true
+      }
+    ]
+  }
 }
 
-output "my_greeting" {
-  value = var.greeting
+output "config_output" {
+  value = var.config
 }
