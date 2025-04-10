1e2c6391a7bd544b748a2434b97457726dfc708f
OliaSalamatina <99215497+OliaSalamatina@users.noreply.github.com>
2025-04-10 09:29:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,31 +11,8 @@ variable "config" {
       enabled = bool
     }))
   })
-
-  default = {
-    metadata = {
-      project     = "internal-dashboard"
-      owner       = "team-devops"
-      department  = "IT"
-      tags        = ["internal", "high-priority"]
-    }
-
-    feature_toggles = [
-      {
-        name    = "enable_user_audit"
-        enabled = true
-      },
-      {
-        name    = "allow_guest_login"
-        enabled = false
-      },
-      {
-        name    = "beta_mode"
-        enabled = true
-      }
-    ]
-  }
 }
+
 
 output "config_output" {
   value = var.config
